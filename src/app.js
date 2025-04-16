0372d208d7af78fdf62a79e5fe5795f2d2a5bddb
barath-dev <barathwaj1153@gmail.com>
2025-04-17 02:43:08 +0530

    added ports

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = require("./routes/auth.route");
 const dotenv = require("dotenv");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 // const { catchAsync } = require("./src/utils/catchAsync");
 // const AppError = require("./src/utils/appError");
 // const globalErrorHandler = require("./src/controllers/error/errorController");
@@ -9,6 +12,8 @@ const db = require("./../config/db");
 
 
 dotenv.config();
+
+
 
 const app = express();
 app.use(express.json());
@@ -32,6 +37,19 @@ app.use("/api/v1/auth", router);
 
 // app.use(globalErrorHandler);
 
-app.listen(process.env.HTTP_PORT || 3000, () => {
-  console.log("Server is running on port 3000");
+http.createServer((req, res) => {
+  res.writeHead(301, { Location: "https://" + req.headers.host + req.url });
+  res.end();
+}).listen(process.env.HTTP_PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.HTTP_PORT || 3000}`);
+});
+
+https.createServer(
+  {
+    key: fs.readFileSync("client-key.pem"),
+    cert: fs.readFileSync("client-cert.pem"),
+  },
+  app
+).listen(process.env.HTTPS_PORT || 443, () => {
+  console.log(`Server running on port ${process.env.HTTPS_PORT || 443}`);
 });
